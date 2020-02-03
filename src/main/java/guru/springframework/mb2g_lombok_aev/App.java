8322e16e7eec00a45462b6fc193bfb141d25fa92
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-02-02 22:23:51 -0300

    final project lombok

---
+++ src/main/java/guru/springframework/mb2g_lombok_aev/App.java
@@ -0,0 +1,13 @@
+package guru.springframework.mb2g_lombok_aev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
